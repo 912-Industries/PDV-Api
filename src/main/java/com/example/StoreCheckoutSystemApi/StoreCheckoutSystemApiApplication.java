1c9c34ea10847b5d6b96c569018b86eaa21855db
Yuri Bueno <yuri.bueno@vrsoft.com.br>
2024-03-12 14:00:26 -0300

    feat: Inicializar projeto API frente de caixa

---
+++ src/main/java/com/example/StoreCheckoutSystemApi/StoreCheckoutSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.StoreCheckoutSystemApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreCheckoutSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreCheckoutSystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StoreCheckoutSystemApi/StoreCheckoutSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StoreCheckoutSystemApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreCheckoutSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
